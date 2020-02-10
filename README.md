47fbdc256d2aedcaff3e610fb902a2d640cff135
Martin Castre <mcastre3@gmail.com>
2020-02-10 17:10:52 -0600

    Use ns template

---
+++ README.md
@@ -0,0 +1,23 @@
+# Maths
+
+> A small Flashcards-style app for children learning counting, addition, subtraction, multiplication, and division. Written in Vue.js + Nativescript.
+
+## Usage
+
+``` bash
+# Install dependencies
+npm install
+
+# Preview on device
+tns preview
+
+# Build, watch for changes and run the application
+tns run
+
+# Build, watch for changes and debug the application
+tns debug <platform>
+
+# Build for production
+tns build <platform> --env.production
+
+```
---
+++ app/components/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to Maths!"/>
+        <GridLayout columns="*" rows="*">
+            <Label class="message" :text="msg" col="0" row="0"/>
+        </GridLayout>
+    </Page>
+</template>
+
+<script lang="ts">
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Rubik:400,700&display=swap');
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20rem;
+        font-family: 'Rubik', sans-serif;
+        color: #fff;
+    }
+</style>
